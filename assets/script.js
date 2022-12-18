8a263f25f3d3d0c2c6f94b1007eae03888be5de6
Saul473 <118771655+Saul473@users.noreply.github.com>
2022-12-18 15:51:48 -0800

    added all of JS except for resetting button w/out refreshing page.

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,69 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
+
+var choices = [];
+var passwordLength = 8;
+var passwordNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', ';', '.', '/', '?', '<', '>'];
+
+
+// 1. Prompt user w/password criteria
+// . choose length of password between 8-128
+// . ask to include: lowercase, uppercase, numeric, and special chars.
+// 2. validate user's response with an alert after each selection
+function generatePassword() {
+
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var random = Math.floor(Math.random() * choices.length);
+    password = password + choices[random];
+  }
+  return password;
+}
+
+
+//prompts
+function getPrompts() {
+  choices = [];
+
+  passwordLength = parseInt(prompt("How long would you like your password to be? \n Please choose a number between 8 - 128."));
+
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert('Password length has to be within 8 - 128 characters in length. \n Try again.');
+    return false;
+  }
+
+  if (confirm('Would you like your password to have numbers?')) {
+    choices = choices.concat(passwordNumbers);
+  }
+
+  if (confirm('Would you like your password to have uppercase letters?')) {
+    choices = choices.concat(uppercaseLetters);
+  }
+
+  if (confirm('Would you like your password to have lowercase letters?')) {
+    choices = choices.concat(lowercaseLetters);
+  }
+
+  if (confirm('Would you like your password to have special characters?')) {
+    choices = choices.concat(specialCharacters);
+  }
+  return true;
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  getPrompts(); 
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  if (getPrompts = true) {
+    var password = generatePassword();
+    passwordText.value = password;
+  } else {
+    passwordText.value = "";
+  }
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
